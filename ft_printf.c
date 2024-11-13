8118162166bc1fad8edc3010aa165902c5295bd6
Paulo Vitor Lopes Meira <pvitor-l@student.42sp.org.br>
2024-11-13 15:46:50 -0300

    version beta function ft_pritf

--- ft_printf.c
+++ ft_printf.c
@@ -6,18 +6,57 @@
 /*   By: pvitor-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 18:21:28 by pvitor-l          #+#    #+#             */
-/*   Updated: 2024/11/13 13:53:27 by pvitor-l         ###   ########.fr       */
+/*   Updated: 2024/11/13 15:46:21 by pvitor-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
 
-static	char	*playholder_type(ch_list)
+static	int	*playholder_type(char c, va_list list)
 {
-	if(c == c )	
-		ft_putchar()
+	int	size;
+
+	size = 0;
+	if(c == 'c')
+		size += ft_putchar(va_arg(list, char));
+	if(c == 's')
+		size += ft_putstr(va_arg(list, char *));
+	if(c == 'p')
+		size += ft_putstr(va_arg(list, char *));
+	if(c == 'd')
+		size += ft_putnbr(va_arg(list, int));
+	if(c == 'i')
+		size += ft_putnbr(va_arg(list, int));
+	if(c == 'u')
+		size += ft_putunbr(va_arg(list, unsigned int));
+	if (c == 'x')
+		size += ft_hexa(va_arg(list, char *));
+	if (c == 'X')
+		size += ft_hexa(va_arg(list, char *));
+	return (size);
 }
+
+int	ft_printf(const char *, ...)
+{
+	
+}
+
+int	main(void)
+{
+	int  x;
+	int  meu;
+
+	x = printf("%d, paulim bacana");
+	meu = ft_ printf("%d, paulim bacana");
+
+	
+	
+}
+		size += ft_putchar(va_arg(list, char));
+
+}
+
 
 int	ft_printf(const char *, ...)
 {
